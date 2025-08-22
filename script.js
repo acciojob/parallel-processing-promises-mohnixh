3d82b74798634fdd0ca386effccb951b36c0aaa1
mohnixh <153740400+mohnixh@users.noreply.github.com>
2025-08-22 18:18:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,34 +12,24 @@ const output = document.getElementById("output");
         function downloadImage(url) {
             return new Promise((resolve, reject) => {
                 const img = new Image();
-                
+                img.src = url;
+
                 img.onload = () => resolve(img);
                 img.onerror = () => reject(`❌ Failed to load image: ${url}`);
-                
-                img.src = url;
             });
         }
 
         function downloadImages() {
-            console.log("downloadImages called");
             output.innerHTML = "";
             errorDiv.innerHTML = "";
             loadingDiv.style.display = "block";
 
-            const promises = imageUrls.map(downloadImage);
-            console.log("Created promises:", promises);
-
-            Promise.all(promises)
+            return Promise.all(imageUrls.map(downloadImage))
                 .then((images) => {
-                    console.log("All images loaded:", images);
                     loadingDiv.style.display = "none";
-                    images.forEach((img) => {
-                        console.log("Appending image:", img.src);
-                        output.appendChild(img);
-                    });
+                    images.forEach((img) => output.appendChild(img));
                 })
                 .catch((err) => {
-                    console.error("Error loading images:", err);
                     loadingDiv.style.display = "none";
                     errorDiv.innerHTML = err;
                 });
